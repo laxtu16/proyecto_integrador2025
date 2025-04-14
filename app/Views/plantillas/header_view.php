a0ec57c92c78be08f930309ee2b7b89c11ebe69f
laxtu16 <leoaquino445@gmail.com>
2025-04-14 16:55:00 -0300

    actualizacion header

--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -8,15 +8,30 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> 
     <link rel="stylesheet" href="assets/css/estilo_footer.css">
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="assets/css/estilos.css"> 
 </head>
 <body>
+
     <header>
-        <a href = "#"  class="logo">
-            <img src="assets\img\logo.png" class="img-thumbnail" width="80" height="80" alt="logo de la compañia">
-            <h2 class="nombre-empresa">KEYS CODI GAMES</h2> 
-        </a>
-        <div> <ul class="nav justify-content-center">
+        <div class="logo">
+            <a href="#">
+                <img src="assets/img/logo.png" class="img-thumbnail" width="80" height="80" alt="logo de la compañia">
+            </a>
+            <h2 class="nombre-empresa"> CODI GAMES</h2> 
+        </div>
+
+        <form class="d-flex my-2 my-lg-0">
+            <input class="form-control me-2" type="search" placeholder="Buscar juegos..." aria-label="Buscar">
+        </form>
+
+
+        <div class = "acciones ">
+             <a  class= "carro-de-compras " href="#"><i class="fas fa-cart-shopping"></i></a>        
+             <a  class= "usuario" href="#"><i class="fas fa-circle-user"></i></a>     
+        </div>
+        <!--
+        <div>
+            <ul class="nav justify-content-center">
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="fa-brands fa-facebook fa-2xl"></i></a>
                 </li>
@@ -30,13 +45,15 @@
                     <a class="nav-link" href="#"><i class="fa-brands fa-youtube"></i></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"> <i class="fa-brands fa-x-twitter"></i></a>
+                    <a class="nav-link" href="#"><i class="fa-brands fa-x-twitter"></i></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"> <i class="fa-brands fa-linkedin"></i></a>
+                    <a class="nav-link" href="#"><i class="fa-brands fa-linkedin"></i></a>
                 </li>
-        
             </ul>
         </div>
+        -->
     </header>
 
+</body>
+</html>
