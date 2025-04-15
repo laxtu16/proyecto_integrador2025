919eafeddcaf4bae7190e442ee503786a2421c59
laxtu16 <leoaquino445@gmail.com>
2025-04-15 00:07:50 -0300

    actualizacion header 2

--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -5,30 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <title>codi games</title>
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> 
+    
     <link rel="stylesheet" href="assets/css/estilo_footer.css">
-    <link rel="stylesheet" href="assets/css/estilos.css"> 
+    <link rel="stylesheet" href="assets/css/estilo_header.css"> 
 </head>
 <body>
 
     <header>
-        <div class="logo">
-            <a href="#">
-                <img src="assets/img/logo.png" class="img-thumbnail" width="80" height="80" alt="logo de la compañia">
-            </a>
-            <h2 class="nombre-empresa"> CODI GAMES</h2> 
+        
+        <div class="container d-flex flex-wrap align-items-center justify-content-between gap-3"> 
+
+            <div class="logo d-flex align-items-center gap-2">
+                <a href="#">
+                    <img src="assets/img/logo.png" class="img-thumbnail" width="80" height="80" alt="logo de la compañia">
+                </a>
+                <h2 class="nombre-empresa">CODI GAMES</h2> 
+            </div>
+
+           
+            <form class="d-flex flex-grow-1 justify-content-center my-2" role="search">
+                <input class="form-control w-100" style="max-width: 400px;" type="search" placeholder="Buscar juegos..." aria-label="Buscar">
+            </form>
+
+           
+            <div class="acciones d-flex align-items-center gap-3">
+                <a class="carro-de-compras" href="#"><i class="fas fa-cart-shopping"></i></a>        
+                <a class="usuario" href="#"><i class="fas fa-circle-user"></i></a>     
+            </div>
+
         </div>
 
-        <form class="d-flex my-2 my-lg-0">
-            <input class="form-control me-2" type="search" placeholder="Buscar juegos..." aria-label="Buscar">
-        </form>
-
-
-        <div class = "acciones ">
-             <a  class= "carro-de-compras " href="#"><i class="fas fa-cart-shopping"></i></a>        
-             <a  class= "usuario" href="#"><i class="fas fa-circle-user"></i></a>     
-        </div>
+        
         <!--
         <div>
             <ul class="nav justify-content-center">
