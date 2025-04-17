053a614535a896be32e7207a3424d67f2b71d99d
laxtu16 <leoaquino445@gmail.com>
2025-04-16 23:22:11 -0300

    Creacion de nav

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('plantillas/header_view.php').view("plantillas/footer_view.php");
+        return view('plantillas/header_view.php').view("plantillas/nav_view.php").view("plantillas/footer_view.php");
     }
 }
--- app/Views/plantillas/nav_view.php
+++ app/Views/plantillas/nav_view.php
@@ -1,3 +1,40 @@
-<nav>
-    
-</nav>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('nosotros'); ?>">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorias
+            </a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="#">Aventura</a></li>
+            <li><a class="dropdown-item" href="#">Estrategia</a></li>
+            <li><a class="dropdown-item" href="#">RPG (Rol)</a></li>
+            <li><a class="dropdown-item" href="#">Simulación</a></li>
+            <li><a class="dropdown-item" href="#">Deportes</a></li>
+            <li><a class="dropdown-item" href="#">Carreras</a></li>
+            <li><a class="dropdown-item" href="#">Multijugador Online</a></li>
+
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div> 
+  </nav>
+  <script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+  
+</div>
