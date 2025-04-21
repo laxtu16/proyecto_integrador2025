5db50fc9202711039f5f0b8efa57b9bbad39b043
BrianAzcona <brianazcona45@gmail.com>
2025-04-21 17:53:49 -0300

    pagina QuienesSomos enrutada

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('plantillas/header_view.php').view("plantillas/nav_view.php").view('plantillas/QuienesSomos.php').view("plantillas/footer_view.php");
+        $data['titulo'] = "Inicio";
+        return view('plantillas/header_view.php', $data).view("plantillas/nav_view.php").view("plantillas/footer_view.php");
+    }
+
+    public function quienesSomos(): string {
+        $data['titulo'] = "Quienes Somos";
+        return view('plantillas/header_view.php', $data).view("plantillas/nav_view.php").view('plantillas/QuienesSomos.php').view("plantillas/footer_view.php");
     }
 }
--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-    <title>codi games</title>
+    <title><?= $titulo ?></title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> 
