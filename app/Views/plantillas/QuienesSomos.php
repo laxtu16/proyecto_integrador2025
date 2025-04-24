2b172d987d0c7a9025c4b5e0c33cb4697a3741f4
BrianAzcona <brianazcona45@gmail.com>
2025-04-24 11:26:49 -0300

    Se inserto el mapa en nosotros

--- app/Views/plantillas/QuienesSomos.php
+++ app/Views/plantillas/QuienesSomos.php
@@ -58,7 +58,11 @@
     </div>
   </div>
 </div>
+<div>
+  <h2 class="info mb-4 text-center">Dónde Encontrarnos!!!</h2>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.589092976526!2d-58.83938682512847!3d-27.482049117231487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456c9b1031ad7f%3A0xf9525d1bc987a704!2sLamadrid%201234%2C%20W3410AOA%20Corrientes!5e0!3m2!1ses!2sar!4v1745504288980!5m2!1ses!2sar" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
